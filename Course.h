990269e16f34aaf93cc72efdf2681dcb244eb55f
Hazem1720 <98549211+Hazem1720@users.noreply.github.com>
2025-11-11 09:48:47 +0200

    Define Course class with student management methods

--- Course.h
+++ Course.h
@@ -4,19 +4,16 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class Course {
+    private:
+    string courseName;
+    string courseCode;
+    int maxStudents;
+   Student* students;
+    int currentStudents;
+  public:
+  
+   void addStudent(const Student& s);
+    void displayCourseInfo();
+};
 #endif
