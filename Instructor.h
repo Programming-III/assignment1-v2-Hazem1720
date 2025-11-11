bcc64cf1a0552c271c86d0dc298c5b14c6d61532
Hazem1720 <98549211+Hazem1720@users.noreply.github.com>
2025-11-11 09:12:02 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,17 +3,11 @@
 
 #include "Person.h"
 using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
-
+class instructor : public person {
+  private:
+    string department;
+    int experienceYears;
+  public:
+    void display();
+};
 #endif
