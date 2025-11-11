af0f8cba5eddc4bdb5a959920ce826845980d484
Hazem1720 <98549211+Hazem1720@users.noreply.github.com>
2025-11-11 09:55:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,45 +4,55 @@
 #include "Student.h"
 #include "Instructor.h"
 #include "Course.h"
-
-
 using namespace std;
 
-// ==================== Person Class Implementation =========================
+ Person::Person(string name,int id){
+this->name=name;
+this->id=id;
+  }
+   void Person::display() {
+       cout << name<<id << endl;
+   } 
+   Student::Student(string major, int yearLevel){
+    this->major=major;
+this->yearLevel=yearLevel;
+   }
+   void Student::display() {
+         cout << major<<yearLevel << endl;
+    }
+    Instructor::Instructor(string department,int experienceYears){
+        this->department=department;
+         this->experienceYears=experienceYears;
+    }
+   void  Instructor:: display() {
+         cout << department<<experienceYears << endl;
+    }
 
-
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
-int main() {
-   
+Course::Course(string courseName,string courseCode,int maxStudents){
+     this->courseName=courseName;
+this->courseCode=courseCode;
+this->maxStudents=maxStudents;
+students[]=new int[maxStudents];
+    currentStudents=0
+}
+void Course:: addStudent(const Student& s){
     
-    return 0;
+    
+students[currentStudents]=s;
+currentStudents++;
+
+}
+void Course:: displayCourseInfo(){
+    cout<<courseName<<courseCode<<maxStudents<<currentStudents<<endl;
+    for(int i=0;i<currentStudents;i++){
+        cout<<students[i]<<endl;
+}
+int main(){
+    Student s =new("cs",19);
+    Course c =new("math","19",5);
+    Instructor i==new("p",7);
+    addStudent(s);
+    c.displayCourseInfo();
+    |s.display()
+    i.display();
 }
