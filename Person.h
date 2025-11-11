6bd60290218dd74e1fc43ca20202b561589f837b
Hazem1720 <98549211+Hazem1720@users.noreply.github.com>
2025-11-11 09:11:00 +0200

    Define Person class with private members and display method

--- Person.h
+++ Person.h
@@ -1,20 +1,13 @@
 #ifndef PERSON_H
 #define PERSON_H
-
 #include <string>
 using namespace std;
+class person{
+  private:
+    string name;
+    int id;
+    public:
+    void display();
 
-//#write Person class here
-
-
-
-
-
-
-
-
-
-
-
-
+};
 #endif
